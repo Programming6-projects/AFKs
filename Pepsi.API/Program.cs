8c028206825bf167cd574b6173d64d743b706c77
Isaias Rojas <82351290+isaias-rojas@users.noreply.github.com>
2024-09-10 21:17:58 -0400

    feat: add basic unit tests

--- Pepsi.API/Program.cs
+++ Pepsi.API/Program.cs
@@ -23,16 +23,15 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-#pragma warning disable CA5394 // Do not use insecure randomness
     var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
+#pragma warning disable CA5394 // Do not use insecure randomness
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
             Random.Shared.Next(-20, 55),
             summaries[Random.Shared.Next(summaries.Length)]
         ))
         .ToArray();
-#pragma warning restore CA5394 // Do not use insecure randomness
     return forecast;
 })
 .WithName("GetWeatherForecast")
@@ -40,7 +39,7 @@ app.MapGet("/weatherforecast", () =>
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+internal class WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
